f5f637134f531936b7d8844b3e9282f48b0bd2ae
Исаева Алена <aleisae@sfedu.ru>
2023-09-30 08:04:51 +0300

    Программа узнаёт високосный год или нет

--- time.pas
+++ time.pas
@@ -1,1 +1,9 @@
-﻿
+﻿begin
+  var x:=ReadInteger('Введите год:');
+  assert(x>0);
+  if ((x mod 4 =0)or(x mod 100 =0))and(x mod 400<>0) then begin
+    print('високосный');
+  end
+  else 
+    print('Не високосный');
+end.
