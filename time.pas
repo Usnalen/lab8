b3d1f41837faece90ed36fe81f6b44f5972a5024
Исаева Алена <aleisae@sfedu.ru>
2023-09-30 08:17:01 +0300

    Добавила кол-во дней в году

--- time.pas
+++ time.pas
@@ -2,8 +2,11 @@
   var x:=ReadInteger('Введите год:');
   assert(x>0);
   if ((x mod 4 =0)or(x mod 100 =0))and(x mod 400<>0) then begin
-    print('високосный');
+    println('Високосный');
+    println('Кол-во дней 366');
   end
-  else 
-    print('Не високосный');
+  else begin
+    println('Не високосный');
+    println('Кол-во дней 365');
+  end; 
 end.
