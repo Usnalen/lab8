989aaa291e4be4183d99bd8a531322cf37a55ff4
Исаева Алена <aleisae@sfedu.ru>
2023-09-30 08:25:15 +0300

    считает кол-во секунд

---
+++ timer.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var x:=ReadInteger('Введите кол-во часов:');
+  var y:=ReadInteger('Введите кол-во минут:');
+  var z:=ReadInteger('Введите кол-во секунд:');
+  assert((x>=0)and(y>=0)and(y<=60)and(z>=0)and(z<=60));
+  Print((x*3600)+(y*60)+z);
+end.
